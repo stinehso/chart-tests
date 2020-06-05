61b64cffdbe05a069aa2a944e08af7fd61ceda5b
Stine <stinehso@gmail.com>
2020-06-05 21:06:16 +0200

    Updates

---
+++ precache-manifest.181cc3e7fac5f0b840a0e49ed93de2be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89e368dac970bb601de2a57f51ae3c89",
+    "url": "/chart-tests/index.html"
+  },
+  {
+    "revision": "e3d908e62bf94c6e0bc2",
+    "url": "/chart-tests/static/css/main.dbac34a0.chunk.css"
+  },
+  {
+    "revision": "df8ffe56bb5fe0e0f138",
+    "url": "/chart-tests/static/js/2.44c8dbfe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chart-tests/static/js/2.44c8dbfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3d908e62bf94c6e0bc2",
+    "url": "/chart-tests/static/js/main.330cc097.chunk.js"
+  },
+  {
+    "revision": "a4cd8019c74b81eb87a9",
+    "url": "/chart-tests/static/js/runtime-main.040faa35.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chart-tests/static/media/logo.5d5d9eef.svg"
+  }
+]);
